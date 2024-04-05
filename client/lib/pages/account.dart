6e9a7b42636808d7dcfb6a6e2d6f2c4ba0bfc72c
sudan <sudan88792@gmail.com>
2024-04-05 23:31:43 +0200

    Esborrat alguns altres fitxers

--- client/lib/pages/account.dart
+++
@@ -1,8 +0,0 @@
-class Account {
-  final String name;
-  final double balance;
-  final int account_number;
-  final bool select;
-
-  Account({required this.name, required this.balance, required this.account_number, required this.select});
-}
