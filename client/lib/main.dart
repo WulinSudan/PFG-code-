da44ca0736bd7a866abc42e3a10bd3c862302201
sudan <sudan88792@gmail.com>
2024-04-06 15:51:32 +0200

    afegir qrscannerpage.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 
 import 'pages/welcomepage.dart';
 import 'pages/login.dart';
+import 'pages/mainpage.dart';
+import 'pages/qrmainpage.dart';
+import 'pages/qrpayment.dart';
+import 'pages/qrscannerpage.dart';
 
 void main() => runApp(MaterialApp(
 
-  initialRoute: '/welcomepage',
+  initialRoute: '/mainpage',
 
 
   routes: {
     '/welcomepage': (context) => WelcomePage(),
     '/login': (context) => Login(),
-    // '/mainpage': (context) => MainPage(username: 'Maria'),
-    // '/qrmainpage': (context) => QrMainPage(username:'Maria'),
-    //'/qrscannerpage': (context) => QRViewExample(),
-    //'/qrpayment': (context) => QrPayment(),
+    '/mainpage': (context) => MainPage(username: 'Maria'),
+    '/qrmainpage': (context) => QrMainPage(username:'Maria'),
+    '/qrscannerpage': (context) => QRViewExample(),
+    '/qrpayment': (context) => QrPayment(),
   },
 ));
