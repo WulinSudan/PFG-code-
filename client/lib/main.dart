e535bfa844e39f5ed6c73908c38003743a40ee37
sudan <sudan88792@gmail.com>
2024-05-05 14:54:33 +0200

    creat fitxer grapgql client i queries

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,33 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'graphql_queries.dart';
+import 'pages/welcomepage.dart';
+import 'pages/login.dart';
 
-const allUsersGraphql = """
-  query allUsers{
-     allUsers {
-        name
-     }
-  }
-""";
 
+import 'graphql_client.dart'; // Importa el archivo que acabas de crear.
 
 void main() {
-  final HttpLink httpLink = HttpLink("http://192.168.1.29:4000/");
-
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      link: httpLink,
-      cache: GraphQLCache(store: InMemoryStore()),
-    ),
-  );
-
-  var app = GraphQLProvider(
-    client: client,
-    child: MyApp(),
-  );
-
-  runApp(app);
+  runApp(createGraphQLProvider());
 }
-
 
 
 class MyApp extends StatefulWidget {
@@ -40,49 +22,13 @@ class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("hola"),
-          ),
-          body: Query(
-            options: QueryOptions(
-              document: gql(allUsersGraphql),
-            ),
-            builder: (QueryResult result, {fetchMore, refetch}) {
-
-              if(result.hasException){
-                print("exception");
-                return Text(result.exception.toString());
-              }
-
-              if(result.isLoading){
-                print("Loading");
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-
-              final userList = result.data?["allUsers"];
-
-              print(userList);
-
-              return Column(
-                children: [
-                  Expanded(
-                    child: GridView.builder(
-                      itemCount: userList.length,
-                      itemBuilder: (_, index){
-                        return Text(userList[index]['name']);
-                      }, 
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                    )
-                  )
-                ],
-              );
-            },
-          )
-        ),
+      initialRoute: '/welcomepage',
+      routes: {
+        '/welcomepage': (context) => WelcomePage(),
+        '/login': (context) => Login(), // Asumiendo que tienes una clase llamada Login para tu pantalla de inicio de sesión.
+      },
     );
   }
 }
+
 
