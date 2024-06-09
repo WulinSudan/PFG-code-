82ffb28d44c2bc7aef8d7393b56cf397bcca0c29
sudan wu <sudan88792@gmail.com>
2024-06-09 15:25:26 +0200

    client: numero de compte mostrat en chargepage.dart

--- client/lib/pages/chargepage.dart
+++ client/lib/pages/chargepage.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ChargePage extends StatefulWidget {
-  final String accountNumber= "123";
-
-  //ChargePage({required this.accountNumber});
-
-  @override
-  _ChargePageState createState() => _ChargePageState();
-}
-
-class _ChargePageState extends State<ChargePage> {
+class ChargePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as Map;
+    final accountNumber = args['accountNumber'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cargar Cuenta'),
+        title: Text('Charge Page'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Número de cuenta seleccionada:',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 10),
-            Text(
-              widget.accountNumber,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+        child: Text('Account Number: $accountNumber'),
       ),
     );
   }
