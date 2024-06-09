84bfec394d1b6db185f926cdf65379746ca0c64a
sudan wu <sudan88792@gmail.com>
2024-06-09 16:30:45 +0200

    client: chargepage.dart feta

--- client/lib/pages/chargepage.dart
+++ client/lib/pages/chargepage.dart
@@ -1,17 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart'; // Asegúrate de tener esta dependencia
 
-class ChargePage extends StatelessWidget {
+class ChargePage extends StatefulWidget {
+  @override
+  _ChargePageState createState() => _ChargePageState();
+}
+
+class _ChargePageState extends State<ChargePage> {
+  late String accountNumber;
+  String qrData = '';
+  TextEditingController amountController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      final args = ModalRoute.of(context)?.settings.arguments as Map?;
+      if (args != null && args.containsKey('accountNumber')) {
+        setState(() {
+          accountNumber = args['accountNumber']!;
+          qrData = 'accountNumber:$accountNumber,importe:0';
+        });
+      }
+    });
+  }
+
+  void _updateQrCode() {
+    final newQrData = 'accountNumber:$accountNumber,importe:${amountController.text}';
+    print('New QR Data: $newQrData'); // Verifica el nuevo valor de qrData
+    setState(() {
+      qrData = newQrData;
+    });
+    print('QR Data Updated'); // Verifica si se actualiza qrData correctamente
+  }
+
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments as Map;
-    final accountNumber = args['accountNumber'];
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Charge Page'),
       ),
-      body: Center(
-        child: Text('Account Number: $accountNumber'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 30.0),
+                QrImageView(
+                  data: qrData,
+                  version: QrVersions.auto,
+                  size: 200.0,
+                ),
+                SizedBox(height: 20),
+                Text('Account Number: ${accountNumber ?? ''}'),
+                SizedBox(height: 20),
+                TextField(
+                  controller: amountController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter the amount',
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _updateQrCode,
+                  child: Text('Update'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
